ef877d5e687ee63142f2543c80753ce9dbdd166c
znu <znu_1@protonmail.com>
2021-04-05 19:41:34 +0200

    feat: add moves animations on small screens

--- client/src/core/components/Team/Card/Moves/Moves.tsx
+++ client/src/core/components/Team/Card/Moves/Moves.tsx
@@ -4,8 +4,6 @@ import styled from 'styled-components';
 
 import useWindowSize from '../../../../hooks/useWindowSize';
 import { breakpoints } from '../../../../../styles/theme/breakpoints';
-import { IoTriangleSharp } from 'react-icons/io5';
-
 import Move from '../../../../models/move';
 import MoveDetail from './MoveDetail';
 
@@ -13,9 +11,16 @@ type HiddenProps = {
   hide: boolean;
 };
 
-const Container = styled.div<HiddenProps>`
+type ContainerProps = {
+  maxHeight: string;
+};
+
+const Container = styled.div<ContainerProps>`
+  transition: max-height 0.8s ease-in-out;
+  max-height: ${(props) => props.maxHeight};
   margin: 1rem auto 0;
-  display: ${(props) => (props.hide ? 'none' : 'grid')};
+  overflow: hidden;
+  display: grid;
   column-gap: 4rem;
   row-gap: 1rem;
   grid-template-columns: repeat(2, 150px [col-start]);
@@ -41,7 +46,10 @@ const Bar = styled.div<HiddenProps>`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  margin-bottom: ${(props) => (props.hide ? 0 : '1rem')};
+
+  &:hover {
+    cursor: pointer;
+  }
 
   @media only screen and (max-width: ${(props) => props.theme.breakpoints.xs}px) {
     font-size: 14px;
@@ -53,12 +61,28 @@ const BarTitle = styled.h3`
   margin: 0;
 `;
 
+type TriangleProps = {
+  rotateAngle: number;
+};
+
+const Triangle = styled.div<TriangleProps>`
+  display: inline-block;
+  height: 0;
+  width: 0;
+  border-right: 8px solid transparent;
+  border-bottom: 14px solid ${(props) => props.theme.primary};
+  border-left: 8px solid transparent;
+  transition: transform 0.8s ease-in-out;
+  transform: rotate(${(props) => props.rotateAngle}deg);
+`;
+
 type MovesProps = {
   moves: Move[];
 };
 
 const Moves = ({ moves }: MovesProps) => {
   const [isHidden, setIsHidden] = useState(true);
+  const [triangleAngle, setTriangleAngle] = useState(360);
   const [isSmallScreen, setIsSmallScreen] = useState(false);
   const windowSizes = useWindowSize();
 
@@ -66,19 +90,22 @@ const Moves = ({ moves }: MovesProps) => {
     setIsSmallScreen(windowSizes.size.width <= breakpoints.s);
   }, [windowSizes]);
 
-  const something = () => {
+  const toggleMoves = () => {
     setIsHidden(!isHidden);
+    setTriangleAngle(triangleAngle - 180);
   };
 
   return (
     <>
       {isSmallScreen && (
-        <Bar hide={isHidden}>
+        <Bar hide={isHidden} onClick={toggleMoves}>
           <BarTitle>Moves</BarTitle>
-          <IoTriangleSharp onClick={something} />
+          <Triangle rotateAngle={triangleAngle} />
         </Bar>
       )}
-      <Container hide={isSmallScreen && isHidden}>
+      <Container
+        maxHeight={!isSmallScreen || (isSmallScreen && !isHidden) ? '1000px' : '0px'}
+      >
         {moves.map((move, index) => {
           // TODO: pass the pokemon id here in key
           return <MoveDetail key={`${move}-${index}`} move={move} />;
